2238ac5c89aa504266fac0698a9a0265c465bdb8
Andrey Rakhmatullin <wrar@wrar.name>
2024-04-19 16:53:38 +0500

    Use the Scrapy fingerprinter.

--- duplicate_url_discarder/middlewares.py
+++ duplicate_url_discarder/middlewares.py
@@ -1,13 +1,19 @@
-import hashlib
+from __future__ import annotations
+
 import os
-from typing import List, Set, Union
+from typing import TYPE_CHECKING, List, Set, Union
 
 from scrapy import Request
 from scrapy.crawler import Crawler
 from scrapy.exceptions import IgnoreRequest, NotConfigured
 from scrapy.http import Response
+from scrapy.utils.request import RequestFingerprinterProtocol
 
 from .url_canonicalizer import UrlCanonicalizer
+
+if TYPE_CHECKING:
+    # typing.Self requires Python 3.11
+    from typing_extensions import Self
 
 
 class DuplicateUrlDiscarderDownloaderMiddleware:
@@ -19,18 +25,22 @@ class DuplicateUrlDiscarderDownloaderMiddleware:
         if not rule_paths:
             raise NotConfigured("No DUD_LOAD_RULE_PATHS set")
         self.url_canonicalizer = UrlCanonicalizer(rule_paths)
-        self._fingerprints: Set[str] = set()
+        assert crawler.request_fingerprinter
+        self._fingerprinter: RequestFingerprinterProtocol = (
+            crawler.request_fingerprinter
+        )
+        self._fingerprints: Set[bytes] = set()
 
-    @staticmethod
-    def _url_fingerprint(url: str) -> str:
-        return hashlib.sha1(url.encode()).hexdigest()
+    @classmethod
+    def from_crawler(cls, crawler: Crawler) -> Self:
+        return cls(crawler)
 
     def process_request(self, request: Request) -> Union[Request, Response, None]:
         if not request.meta.get("dud", False):
             self.crawler.stats.inc_value("duplicate_url_discarder/request/skipped")
             return None
         canonical_url = self.url_canonicalizer.process_url(request.url)
-        fp = self._url_fingerprint(canonical_url)
+        fp = self._fingerprinter.fingerprint(request.replace(url=canonical_url))
         if fp in self._fingerprints:
             self.crawler.stats.inc_value("duplicate_url_discarder/request/discarded")
             raise IgnoreRequest(f"Duplicate URL discarded: {canonical_url}")
